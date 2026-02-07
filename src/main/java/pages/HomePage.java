4c121469046fee8d2bf7d827b635ebd885702324
Tamara <vovktamars@gmail.com>
2026-02-07 16:09:12 +0300

    add loggerUtil update HomePage update HomeTest

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -2,26 +2,50 @@ package pages;
 
 import driver.*;
 import org.openqa.selenium.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 public class HomePage {
+
     private final String HOME_URL = "https://chitatel.by/";
+
     private final By LOGIN_BUTTON = By.xpath("//div[@class=\"name\" and text()=\"Вход\"]");
+    private final By SEARCH_BAR = By.xpath(" //input[@id=\"input-search\"]");
+    private final By LEGAL_ENTITY = By.xpath(" //div[@class=\"f-name\" and text()=\\\"Общество с ограниченной ответственностью «Абрис-Бел»\\\"]");
 
     private WebDriver driver;
 
     public HomePage() {
         this.driver = Driver.getDriver();
+
     }
 
-    public void open() {
+    public void openHomePage() {
         driver.get(HOME_URL);
     }
-    public String getHome_Url(){
+
+    public String getHomePageUrl() {
         return HOME_URL;
     }
 
-    public void clickLogin() {
+
+    public boolean isLoginButtonDisplayed() {
+        return driver.findElement(LOGIN_BUTTON).isDisplayed();
+    }
+
+    public void isLoginButtonEnable() {
         driver.findElement(LOGIN_BUTTON).click();
     }
+
+
+    public boolean isSearchBarDisplayed() {
+        return driver.findElement(SEARCH_BAR).isDisplayed();
+    }
+
+    public void isSearchBarEnable() {
+        driver.findElement(SEARCH_BAR).click();
+    }
+
 
 }
---
+++ src/test/java/HomePageTest.java
@@ -0,0 +1,67 @@
+import driver.Driver;
+import org.apache.logging.log4j.*;
+import pages.HomePage;
+import org.junit.jupiter.api.*;
+import utils.*;
+
+public class HomePageTest {
+
+    private static final Logger logger = LoggerUtil.getlogger(HomePageTest.class);
+    private HomePage homePage;
+
+    @BeforeEach
+    public void openHomePage() {
+        System.out.println("BEFORE EACH START");
+        logger.info("Начало подготовки теста");
+        homePage = new HomePage();
+        homePage.openHomePage();
+        logger.info("Главня страница открыта");
+    }
+
+
+    @Test
+    public void homePageShouldOpenWithCorrectUrl() {
+        logger.info("Тест: homePageShouldOpenWithCorrectUrl");
+        String actualUrl = Driver.getDriver().getCurrentUrl();
+        Assertions.assertEquals(homePage.getHomePageUrl(), actualUrl, "Некорректный URL главной страницы");
+        logger.info("URL главной страницы корректный");
+    }
+
+    @Test
+    public void loginButtonShouldBeDisplayed() {
+        logger.info("Тест: loginButtonShouldBeDisplayed");
+        Assertions.assertTrue(homePage.isLoginButtonDisplayed(), "Кнопка входа не отображается");
+        logger.info("Кнопка входа отображается");
+    }
+
+    @Test
+    public void loginButtonShouldBeEnable() {
+        logger.info("Тест: loginButtonShouldBeEnabled");
+        Assertions.assertDoesNotThrow(() -> {
+            homePage.isLoginButtonEnable();
+            logger.info("Кнопка входа активна");
+        });
+    }
+
+    @Test
+    public void searchBarShouldBeDisplayed() {
+        logger.info("Тест: searchBarShouldBeDisplayed");
+        Assertions.assertTrue(homePage.isSearchBarDisplayed(), "Строка поиска не отображается");
+        logger.info("Строка поиска отображается");
+    }
+
+    @Test
+    public void searchBarShouldBeEnabled() {
+        logger.info("Тест: searchBarShouldBeEnabled");
+        Assertions.assertDoesNotThrow(() -> {
+            homePage.isSearchBarEnable();
+        });
+        logger.info("Строка поиска активна");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        logger.info("Окончание теста");
+        Driver.quit();
+    }
+}
